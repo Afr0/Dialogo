dbfa98bb3f6e4191fd6e4369236dff10633b1db5
Mats Vederhus <afr088@hotmail.com>
2024-03-10 17:16:20 +0100

    Added more event handlers to index view.

--- public/Views/IndexView.js
+++ public/Views/IndexView.js
@@ -4,24 +4,121 @@ import LanguageManager from "../LanguageManager.js";
 export default class IndexView extends DialogoView {
     #columnClass;
     #languageManager;
+    
+    #btnLogin;
+    #btnLogout;
+    #btnNewUser;
+    #btnStart;
+    #btnSettings;
 
-    /**Constructs a new instance of IndexView.
+    #navigateToLoginEvent;
+    #logoutEvent
+    #navigateToSettingsEvent;
+    #navigateToCreateUserEvent;
+    #navigateToLanguageSelectionEvent;
+
+    /**Constructs a new IndexView instance.
+     * Please use IndexView.createInstance() to create an instance in a consumer class!
+     * @param {string} [viewId=""] The id of the view to create.
+     * @param {*} templateContent The template content for the view to insert into index.html.
      */
-    constructor(viewID) {
+    constructor(viewID="") {
         super(viewID);
 
+        this.#btnStart = document.getElementById("btnStart");
+        if (this.#btnStart) {
+            this.#btnStart.addEventListener("click", () => {
+                if (this.#navigateToLanguageSelectionEvent)
+                    this.#navigateToLanguageSelectionEvent();
+            });
+        }
+
+        this.#btnNewUser = document.getElementById("btnNewUser");
+        LanguageManager.getTranslation("newuser").then((translation) => {
+            this.#btnNewUser.textContent = translation;
+        });
+
+        if (this.#btnNewUser) {
+            this.#btnNewUser.addEventListener("click", () => {
+                if (this.#navigateToCreateUserEvent)
+                    this.#navigateToCreateUserEvent();
+            });
+        }
+        
+        this.#btnLogin = document.getElementById("btnLogin");
+        LanguageManager.getTranslation("login").then((translation) => {
+            this.#btnLogin.textContent = translation;
+        });
+
+        if (this.#btnLogin) {
+            this.#btnLogin.addEventListener("click", () => {
+                if (this.#navigateToLoginEvent)
+                    this.#navigateToLoginEvent();
+            });
+        }
+
+        this.#btnLogout = document.getElementById("btnLogout");
+        LanguageManager.getTranslation("logout").then((translation) => {
+            this.#btnLogout.textContent = translation;
+        });
+
+        if (this.#btnLogout) {
+            this.#btnLogout.addEventListener("click", () => {
+                if (this.#logoutEvent)
+                    this.#logoutEvent();
+            });
+        }
+
+        this.#btnSettings = document.getElementById("btnSettings");
+        LanguageManager.getTranslation("settings").then((translation) => {
+            this.#btnSettings.textContent = translation;
+        });
+
+        if(this.#btnSettings) {
+            this.#btnSettings.addEventListener("click", () => {
+                if(this.#navigateToSettingsEvent)
+                    this.#navigateToSettingsEvent();
+            });
+        }
+
         this.#languageManager = new LanguageManager();
-        
-        let btnNewUser = document.getElementById("btnNewUser");
-        btnNewUser.addEventListener("click", () => {
-            this.emit("navigateToCreateUser");
-        });
-        
-        let btnLogin = document.getElementById("btnLogin");
-        btnLogin.addEventListener("click", () => {
-            window.location.href = "./login.html";
-        });
+    }
+    
+    /**Event handler for assigning a callback to the navigateToLanguageSelection event. */
+    onNavigatingToLanguageSelection(callback) {
+        this.#navigateToLanguageSelectionEvent = callback;
+    }
+    
+    /**Event handler for assigning a callback to the navigateToLogin event. */
+    onNavigatingToLogin(callback) {
+        this.#navigateToLoginEvent = callback;
     }
 
+    /**Event handler for assigning a callback to the logout event. */
+    onLogout(callback) {
+        this.#logoutEvent = callback;
+    }
+
+    /**Event handler for assigning a callback to the navigateToCreateUser event. */
+    onNavigatingToCreateuser(callback) {
+        this.#navigateToCreateUserEvent = callback;
+    }
+
+    /**Event handler for assigning a callback to the navigateToSettings event. */
+    onNavigatingToSettings(callback) {
+        this.#navigateToSettingsEvent = callback;
+    }
+
+    /**Switches the hidden properties of the login and logout buttons. */
+    switchLoginBtn() {
+        this.#btnLogout = document.getElementById("btnLogout");
+
+        if(this.#btnLogout.hidden) {
+            this.#btnLogout.hidden = false;
+            this.#btnLogin.hidden = true;
+        } else {
+            this.#btnLogout.hidden = true;
+            this.#btnLogin.hidden = false;
+        }
     }
  }
